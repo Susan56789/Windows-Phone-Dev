c7d65c9e8901b85f806fc714ef22bb37835145f2
Susan Wairimu <59855870+Susan56789@users.noreply.github.com>
2020-09-06 07:12:52 +0300

    Add files via upload
    Interest Calculator

---
+++ MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using SimpleCalc.Resources;
+
+namespace SimpleCalc
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            double amount;
+            int duration;
+            double interest;
+            double tAmount;
+            amount =Int32.Parse(txtAmount.Text);
+            duration = Int32.Parse(txtDuration.Text);
+            interest = 1.5;
+
+            tAmount = (interest / 100 * amount * duration) + amount;
+
+            if (duration <= 0)
+            {
+                MessageBox.Show("Duration cannot be zero");
+                txtDuration.Focus();
+                return;
+            }
+
+            MessageBox.Show("Total amount of money to be paid after"+" " + duration + " " + "months is"+ " " +tAmount );
+
+           
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
